486d6015337e4f5a4507543cae5f6f1f11f42fce
Brandon Nguyen <gnuyent@protonmail.com>
2021-04-17 00:11:07 -0700

    refactor: simplify professor matching logic

--- src/scheduler/controller.rs
+++ src/scheduler/controller.rs
@@ -1,31 +1,59 @@
 use crate::scraper::{Options, SDSUSpider};
 use itertools::Itertools;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::iter::FromIterator;
 
-use super::{schedule::Schedule, Parameters};
+use super::{schedule::Schedule, Course, Parameters};
 
 pub struct Controller {
+    courses: HashMap<String, Vec<Course>>,
     schedules: Vec<Schedule>,
     parameters: Parameters,
 }
 
+// FIXME: Error handle if 0 schedules.
+// for course in &schedules[schedules.len() - 1].courses {
+//     write!(
+//         "Course: {}-{} ({}) - {}",
+//         course.course_subject, course.course_number, course.schedule_num, course.url
+//     );
+//     for meeting in course.meetings.iter() {
+//         write!(
+//             "    This is a {} taught by {:?} {} on {:?} between {}{}-{}{}.",
+//             meeting.meeting_type,
+//             meeting.instructor,
+//             meeting.location,
+//             meeting.date.day,
+//             meeting.date.start_time.hour(),
+//             meeting.date.start_time.minute(),
+//             meeting.date.end_time.hour(),
+//             meeting.date.end_time.minute(),
+//         );
+//     }
+//     write!(
+//         "    There are {}/{} seats available.\n",
+//         course.seats_available, course.seats_total
+//     );
+// }
+
 impl Controller {
     pub fn new(parameters: Parameters) -> Self {
+        let courses = match parameters.school.as_ref() {
+            "SDSU" => SDSUSpider::new(Options::from_params(&parameters)).parse(),
+            _ => panic!("Unable to create a spider."),
+        }
+        .unwrap();
+
         Self {
+            courses,
             schedules: Vec::new(),
             parameters,
         }
     }
 
-    pub fn generate_schedules(&self) {
-        let courses = match self.parameters.school.as_ref() {
-            "SDSU" => SDSUSpider::new(Options::from_params(&self.parameters)).parse(),
-            _ => panic!("Unable to create a spider."),
-        }
-        .unwrap();
-
-        let schedules_raw = courses
+    pub fn generate_schedules(&mut self) {
+        let schedules_raw = self
+            .courses
             .values()
             .into_iter()
             .map(IntoIterator::into_iter)
@@ -34,13 +62,11 @@ impl Controller {
 
         info!("Generated {} schedules.", schedules_raw.len());
 
-        let mut schedules: Vec<Schedule> = Vec::new();
-
         for schedule in schedules_raw {
             let mut s = Schedule::new(schedule);
             if s.is_valid() {
                 s.calculate_fitness(&self.parameters);
-                schedules.push(s);
+                self.schedules.push(s);
             }
         }
 
@@ -58,56 +84,27 @@ impl Controller {
                     .map(|p| p.to_uppercase()),
             );
             // Only include schedules where it has at least one instructor matching the given list.
-            schedules = schedules
-                .into_iter()
-                .filter(|s| {
-                    HashSet::from_iter(s.courses.iter().flat_map(|c| c.meetings.iter()).map(|m| {
-                        // Create a set with all of this schedule's instructors.
-                        m.instructor
-                            .clone()
-                            .split_whitespace()
-                            .nth(1)
-                            .unwrap()
-                            .to_uppercase()
-                    }))
-                    // Perform a set intersection between this schedule's instructors and the
-                    // configuration.
-                    .intersection(&professors)
-                    .collect::<HashSet<_>>()
-                    // Check if it includes the some or all of the professors.
-                    .len()
-                        >= num_matches
-                })
-                .collect();
+            self.schedules.retain(|s| {
+                HashSet::from_iter(s.courses.iter().flat_map(|c| c.meetings.iter()).map(|m| {
+                    // Create a set with all of this schedule's instructors.
+                    m.instructor
+                        .clone()
+                        .split_whitespace()
+                        .nth(1)
+                        .unwrap()
+                        .to_uppercase()
+                }))
+                // Perform a set intersection between this schedule's instructors and the
+                // configuration.
+                .intersection(&professors)
+                // Check if it includes the some or all of the professors.
+                .count()
+                    >= num_matches
+            })
         }
 
-        info!("Validated {} schedules.", schedules.len());
-        // FIXME: Error handle if 0 schedules.
+        info!("Validated {} schedules.", self.schedules.len());
 
-        schedules.sort_by(|s1, s2| s1.fitness.cmp(&s2.fitness));
-
-        for course in &schedules[schedules.len() - 1].courses {
-            println!(
-                "Course: {}-{} ({}) - {}",
-                course.course_subject, course.course_number, course.schedule_num, course.url
-            );
-            for meeting in course.meetings.iter() {
-                println!(
-                    "    This is a {} taught by {:?} {} on {:?} between {}{}-{}{}.",
-                    meeting.meeting_type,
-                    meeting.instructor,
-                    meeting.location,
-                    meeting.date.day,
-                    meeting.date.start_time.hour(),
-                    meeting.date.start_time.minute(),
-                    meeting.date.end_time.hour(),
-                    meeting.date.end_time.minute(),
-                );
-            }
-            println!(
-                "    There are {}/{} seats available.\n",
-                course.seats_available, course.seats_total
-            );
-        }
+        self.schedules.sort_by(|s1, s2| s1.fitness.cmp(&s2.fitness));
     }
 }
